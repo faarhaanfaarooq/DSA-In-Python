158e034e3fbf44f5e6b29d828172bb207bd46b59
faarhaanfaarooq <faarhaanfaarooq@gmail.com>
2024-02-14 22:36:07 +0600

    .

--- Arrays/bubbleSort.py
+++ Arrays/bubbleSort.py
@@ -1,7 +1,11 @@
 myArray = [10, 20, 3, 4, 1, 100, 0]
 n = len(myArray)
 for i in range(n-1):
+    swapped = False
     for j in range(n-i-1):
         if myArray[j] > myArray[j+1]:
             myArray[j], myArray[j+1] = myArray[j+1], myArray[j]
+            swapped = True
+    if not swapped:
+        break
 print(myArray)
